ff2e473584d71baf27694f8bda5b8625222eaed6
Mathias L. Baumann <mathias.baumann@sociomantic.com>
2017-06-12 16:11:09 +0200

    Add module to create oauth tokens

---
+++ src/octod/api/authorizations.d
@@ -0,0 +1,58 @@
+/*******************************************************************************
+
+    Provides wrappers on top of some methods documented in
+    https://developer.github.com/v3/oauth_authorizations/
+
+    Copyright: Copyright (c) 2016 sociomantic labs GmbH. All rights reserved
+
+    License: Boost Software License Version 1.0 (see LICENSE for details)
+
+*******************************************************************************/
+
+module octod.api.authorizations;
+
+import std.exception : enforce;
+import vibe.data.json;
+import octod.core;
+import octod.api.common;
+
+/*******************************************************************************
+
+    Uses the given user/pass to create a connection which is then used to setup
+    an oauth token with the given scopes and note
+
+    Params:
+        user = github username
+        pass = github password
+        scopes = oauth permission scopes
+        note = oauth note
+
+    Returns:
+        oauth token code
+
+*******************************************************************************/
+
+public string createOAuthToken ( string user, string pass,
+                                 string[] scopes, string note )
+{
+    Configuration cfg;
+
+    cfg.dryRun = false;
+    cfg.username = user;
+    cfg.password = pass;
+
+    auto connection = HTTPConnection.connect(cfg);
+
+    Json json = Json.emptyObject;
+
+    json["scopes"] = Json.emptyArray;
+
+    foreach (_scope; scopes)
+        json["scopes"] ~= _scope;
+
+    json["note"] = note;
+
+    auto response = connection.post("/authorizations", json);
+
+    return response["token"].to!string;
+}
